da2cfb89628d87e50384eaed76b034e2ecf3d396
Ling Zhang <dearlingzhang@gmail.com>
2018-11-25 23:20:32 -0500

    sem_seg add down sampling

--- sem_seg/model.py
+++ sem_seg/model.py
@@ -19,12 +19,12 @@ def placeholder_inputs(batch_size, num_point):
 
 def model_part(point_cloud, is_training, k, bn_decay=None):
       out1 = tf_util.conv2d(point_cloud, 64, [1,1],
-                           padding='VALID', stride=[2,1],
+                           padding='VALID', stride=[1,1],
                            bn=True, is_training=is_training,
                            scope='samp_conv1', bn_decay=bn_decay, is_dist=True)
 
       out2 = tf_util.conv2d(out1, 64, [1,1],
-                           padding='VALID', stride=[2,1],
+                           padding='VALID', stride=[1,1],
                            bn=True, is_training=is_training,
                            scope='samp_conv2', bn_decay=bn_decay, is_dist=True)
 
@@ -41,7 +41,7 @@ def model_part(point_cloud, is_training, k, bn_decay=None):
       edge_feature = tf_util.get_edge_feature(out3, nn_idx=nn_idx, k=k)
 
       out4 = tf_util.conv2d(edge_feature, 64, [1,1],
-                           padding='VALID', stride=[2,1],
+                           padding='VALID', stride=[1,1],
                            bn=True, is_training=is_training,
                            scope='samp_conv4', bn_decay=bn_decay, is_dist=True)
 
@@ -71,7 +71,7 @@ def model_part(point_cloud, is_training, k, bn_decay=None):
                            scope='samp_conv7', bn_decay=bn_decay, is_dist=True)
       print("out7 = ", out7.shape)
       out8 = tf_util.conv2d(out7, 1024, [1, 1],
-                           padding='VALID', stride=[2,1],
+                           padding='VALID', stride=[1,1],
                            bn=True, is_training=is_training,
                            scope='samp_conv8', bn_decay=bn_decay, is_dist=True)
       print("out8 = ", out8.shape)
@@ -92,7 +92,7 @@ def get_model(point_cloud, is_training, bn_decay=None):
   nn_idx = tf_util.knn(adj, k=k) # (batch, num_points, k)
   edge_feature = tf_util.get_edge_feature(input_image, nn_idx=nn_idx, k=k)
 
-  globle_net = model_part(edge_feature, is_training, k, bn_decay)
+  globle_feat = model_part(edge_feature, is_training, k, bn_decay)
 
   out1 = tf_util.conv2d(edge_feature, 64, [1,1],
                        padding='VALID', stride=[1,1],
@@ -166,6 +166,10 @@ def get_model(point_cloud, is_training, bn_decay=None):
                                      net_mean_3,
                                      out7,
                                      out8])
+  # CONCAT
+  # pc_feat1_expand = tf.tile(tf.reshape(pc_feat1, [batch_size, 1, 1, -1]), [1, num_point, 1, 1])
+  points_feat1_concat = tf.concat(axis=3, values=[globle_feat, concat])
+  print("points_feat1_concat = ", points_feat1_concat.shape)
 
   # CONV
   net = tf_util.conv2d(concat, 512, [1,1], padding='VALID', stride=[1,1],
