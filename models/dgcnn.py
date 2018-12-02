2615c13f56a31e6422dcae9cb58e28593b667073
Ling Zhang <dearlingzhang@gmail.com>
2018-12-01 21:28:32 -0500

    add sampling to class using structure 5 v2

--- models/dgcnn.py
+++ models/dgcnn.py
@@ -65,18 +65,19 @@ def get_model(point_cloud, is_training, bn_decay=None):
   net = tf.reduce_max(net, axis=-2, keep_dims=True)
   net3 = net
 
+  # down sampling
+
   adj_matrix = tf_util.pairwise_distance(net)
   nn_idx = tf_util.knn(adj_matrix, k=k)
   edge_feature = tf_util.get_edge_feature(net, nn_idx=nn_idx, k=k)
 
   net = tf_util.conv2d(edge_feature, 128, [1,1],
-                       padding='VALID', stride=[1,1],
+                       padding='VALID', stride=[2,1],
                        bn=True, is_training=is_training,
                        scope='dgcnn4', bn_decay=bn_decay)
   net = tf.reduce_max(net, axis=-2, keep_dims=True)
   net4 = net
 
-  # down sampling
   adj_matrix = tf_util.pairwise_distance(net)
   nn_idx = tf_util.knn(adj_matrix, k=k)
   edge_feature = tf_util.get_edge_feature(net, nn_idx=nn_idx, k=k)
@@ -96,34 +97,17 @@ def get_model(point_cloud, is_training, bn_decay=None):
                        padding='VALID', stride=[2,1],
                        bn=True, is_training=is_training,
                        scope='dgcnn6', bn_decay=bn_decay)
-  net = tf.reduce_max(net, axis=-2, keep_dims=True)
-  net6 = net
-
-  adj_matrix = tf_util.pairwise_distance(net)
-  nn_idx = tf_util.knn(adj_matrix, k=k)
-  edge_feature = tf_util.get_edge_feature(net, nn_idx=nn_idx, k=k)
-
-  net = tf_util.conv2d(edge_feature, 512, [1,1],
-                       padding='VALID', stride=[2,1],
+  net = tf_util.conv2d(net, 512, [1,1],
+                       padding='VALID', stride=[1,1],
                        bn=True, is_training=is_training,
                        scope='dgcnn7', bn_decay=bn_decay)
   net = tf.reduce_max(net, axis=-2, keep_dims=True)
-  net7 = net
+  net6 = net
 
-  adj_matrix = tf_util.pairwise_distance(net)
-  nn_idx = tf_util.knn(adj_matrix, k=k)
-  edge_feature = tf_util.get_edge_feature(net, nn_idx=nn_idx, k=k)
-
-  net = tf_util.conv2d(edge_feature, 512, [1,1],
-                       padding='VALID', stride=[1,1],
-                       bn=True, is_training=is_training,
-                       scope='dgcnn8', bn_decay=bn_decay)
-  net = tf.reduce_max(net, axis=-2, keep_dims=True)
- 
   net = tf.reduce_max(net, axis=1, keep_dims=True)
   globle_feat = net
 
-  net = tf_util.conv2d(tf.concat([net1, net2, net3, net4], axis=-1), 1024, [1, 1],
+  net = tf_util.conv2d(tf.concat([net1, net2, net3], axis=-1), 512, [1, 1],
                        padding='VALID', stride=[1,1],
                        bn=True, is_training=is_training,
                        scope='agg', bn_decay=bn_decay)
