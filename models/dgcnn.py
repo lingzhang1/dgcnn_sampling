d175283482b249b45b58be8e6ef78c008771cd96
Ling Zhang <dearlingzhang@gmail.com>
2018-12-03 14:21:24 -0500

    add fully connected layer to class using s6

--- models/dgcnn.py
+++ models/dgcnn.py
@@ -37,7 +37,7 @@ def get_model(point_cloud, is_training, bn_decay=None):
   edge_feature = tf_util.get_edge_feature(point_cloud_transformed, nn_idx=nn_idx, k=k)
 
   net = tf_util.conv2d(edge_feature, 64, [1,1],
-                       padding='VALID', stride=[4,1],
+                       padding='VALID', stride=[1,1],
                        bn=True, is_training=is_training,
                        scope='dgcnn1', bn_decay=bn_decay)
   net = tf.reduce_max(net, axis=-2, keep_dims=True)
@@ -50,6 +50,10 @@ def get_model(point_cloud, is_training, bn_decay=None):
                        padding='VALID', stride=[4,1],
                        bn=True, is_training=is_training,
                        scope='dgcnn2', bn_decay=bn_decay)
+  net = tf_util.conv2d(edge_feature, 64, [1,1],
+                       padding='VALID', stride=[1,1],
+                       bn=True, is_training=is_training,
+                       scope='dgcnn3', bn_decay=bn_decay)
   net = tf.reduce_max(net, axis=-2, keep_dims=True)
 
   adj_matrix = tf_util.pairwise_distance(net)
@@ -59,7 +63,11 @@ def get_model(point_cloud, is_training, bn_decay=None):
   net = tf_util.conv2d(edge_feature, 64, [1,1],
                        padding='VALID', stride=[4,1],
                        bn=True, is_training=is_training,
-                       scope='dgcnn3', bn_decay=bn_decay)
+                       scope='dgcnn4', bn_decay=bn_decay)
+  net = tf_util.conv2d(edge_feature, 64, [1,1],
+                       padding='VALID', stride=[1,1],
+                       bn=True, is_training=is_training,
+                       scope='dgcnn4', bn_decay=bn_decay)
   net = tf.reduce_max(net, axis=-2, keep_dims=True)
 
   # MLP on global point cloud vector
