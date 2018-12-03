2153cc9db48f252ec0b86d42e81900efecfaae19
Ling Zhang <dearlingzhang@gmail.com>
2018-12-02 23:42:09 -0500

    add fully connected layer to class using s6

--- models/dgcnn.py
+++ models/dgcnn.py
@@ -37,11 +37,6 @@ def get_model(point_cloud, is_training, bn_decay=None):
   edge_feature = tf_util.get_edge_feature(point_cloud_transformed, nn_idx=nn_idx, k=k)
 
   net = tf_util.conv2d(edge_feature, 64, [1,1],
-                       padding='VALID', stride=[1,1],
-                       bn=True, is_training=is_training,
-                       scope='dgcnn1', bn_decay=bn_decay)
-
-  net = tf_util.conv2d(edge_feature, 64, [1,1],
                        padding='VALID', stride=[2,1],
                        bn=True, is_training=is_training,
                        scope='dgcnn2', bn_decay=bn_decay)
