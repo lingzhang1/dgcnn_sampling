6cae4d5388b6362ceefc68bc1950ad61d6d20e8a
Ling Zhang <dearlingzhang@gmail.com>
2018-12-04 14:07:54 -0500

    s7_v2 stride conv

--- models/dgcnn.py
+++ models/dgcnn.py
@@ -47,8 +47,8 @@ def get_model(point_cloud, is_training, bn_decay=None):
   nn_idx = tf_util.knn(adj_matrix, k=k)
   edge_feature = tf_util.get_edge_feature(net, nn_idx=nn_idx, k=k)
 
-  net = tf_util.conv2d(edge_feature, 64, [1,1],
-                       padding='VALID', stride=[1,1],
+  net = tf_util.conv2d(edge_feature, 128, [1,1],
+                       padding='VALID', stride=[2,1],
                        bn=True, is_training=is_training,
                        scope='dgcnn2', bn_decay=bn_decay)
   net = tf.reduce_max(net, axis=-2, keep_dims=True)
@@ -58,8 +58,8 @@ def get_model(point_cloud, is_training, bn_decay=None):
   nn_idx = tf_util.knn(adj_matrix, k=k)
   edge_feature = tf_util.get_edge_feature(net, nn_idx=nn_idx, k=k)
 
-  net = tf_util.conv2d(edge_feature, 64, [1,1],
-                       padding='VALID', stride=[1,1],
+  net = tf_util.conv2d(edge_feature, 512, [1,1],
+                       padding='VALID', stride=[2,1],
                        bn=True, is_training=is_training,
                        scope='dgcnn3', bn_decay=bn_decay)
   net = tf.reduce_max(net, axis=-2, keep_dims=True)
@@ -69,14 +69,14 @@ def get_model(point_cloud, is_training, bn_decay=None):
   nn_idx = tf_util.knn(adj_matrix, k=k)
   edge_feature = tf_util.get_edge_feature(net, nn_idx=nn_idx, k=k)
 
-  net = tf_util.conv2d(edge_feature, 128, [1,1],
+  net = tf_util.conv2d(edge_feature, 512, [1,1],
                        padding='VALID', stride=[1,1],
                        bn=True, is_training=is_training,
                        scope='dgcnn4', bn_decay=bn_decay)
   net = tf.reduce_max(net, axis=-2, keep_dims=True)
   net4 = net
 
-  net = tf_util.conv2d(tf.concat([net1, net2, net3, net4], axis=-1), 1024, [1, 1],
+  net = tf_util.conv2d(net, 1024, [1, 1],
                        padding='VALID', stride=[1,1],
                        bn=True, is_training=is_training,
                        scope='agg', bn_decay=bn_decay)
