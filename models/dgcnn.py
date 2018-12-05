e19ee6fd76a724d3428865ccb84565c1aeba0bf0
Ling Zhang <dearlingzhang@gmail.com>
2018-12-05 15:12:25 -0500

    s7_v2 point, point-neighbors k = 6

--- models/dgcnn.py
+++ models/dgcnn.py
@@ -22,7 +22,7 @@ def get_model(point_cloud, is_training, bn_decay=None):
   batch_size = point_cloud.get_shape()[0].value
   num_point = point_cloud.get_shape()[1].value
   end_points = {}
-  k = 6
+  k = 20
 
   adj_matrix = tf_util.pairwise_distance(point_cloud)
   nn_idx = tf_util.knn(adj_matrix, k=k)
@@ -48,7 +48,7 @@ def get_model(point_cloud, is_training, bn_decay=None):
   edge_feature = tf_util.get_edge_feature(net, nn_idx=nn_idx, k=k)
 
   net = tf_util.conv2d(edge_feature, 64, [1,1],
-                       padding='VALID', stride=[2,1],
+                       padding='VALID', stride=[1,1],
                        bn=True, is_training=is_training,
                        scope='dgcnn2', bn_decay=bn_decay)
   net = tf.reduce_max(net, axis=-2, keep_dims=True)
@@ -58,8 +58,8 @@ def get_model(point_cloud, is_training, bn_decay=None):
   nn_idx = tf_util.knn(adj_matrix, k=k)
   edge_feature = tf_util.get_edge_feature(net, nn_idx=nn_idx, k=k)
 
-  net = tf_util.conv2d(edge_feature, 128, [1,1],
-                       padding='VALID', stride=[2,1],
+  net = tf_util.conv2d(edge_feature, 64, [1,1],
+                       padding='VALID', stride=[1,1],
                        bn=True, is_training=is_training,
                        scope='dgcnn3', bn_decay=bn_decay)
   net = tf.reduce_max(net, axis=-2, keep_dims=True)
@@ -76,7 +76,7 @@ def get_model(point_cloud, is_training, bn_decay=None):
   net = tf.reduce_max(net, axis=-2, keep_dims=True)
   net4 = net
 
-  net = tf_util.conv2d(net, 1024, [1, 1],
+  net = tf_util.conv2d(tf.concat([net1, net2, net3, net4], axis=-1), 1024, [1, 1],
                        padding='VALID', stride=[1,1],
                        bn=True, is_training=is_training,
                        scope='agg', bn_decay=bn_decay)
