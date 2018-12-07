6b5a94c8a7f0e238216204144a02bd5f803a4432
Ling Zhang <dearlingzhang@gmail.com>
2018-12-07 11:20:25 -0500

    change epoch

--- train.py
+++ train.py
@@ -134,6 +134,7 @@ def train():
         config.log_device_placement = False
         sess = tf.Session(config=config)
         saver.restore(sess, tf.train.latest_checkpoint('./log_s7_v2_k5_250'))
+        graph = tf.get_default_graph()
 
         log_string("Model restored.")
 
